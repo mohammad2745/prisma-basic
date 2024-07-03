7b08a1ab548f05a0a00ed2ffd2fd0458499b9d49
mohammad2745 <mohammadfcc2745@gmail.com>
2024-07-03 20:05:07 +0600

    prisma install and add in node project using postgres

--- script.ts
+++ script.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-
+  const user = await prisma.user.create({ data: { name: 'Jon' } });
+  console.log(user);
 }
 
 main()
